11c5a534cb1558d523b48922dde503348dd83d56
selbadry25 <elbadrysara531@gmail.com>
2023-03-16 13:43:49 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,13 @@
 
 /**
  * main - prints "Programming is like building a multilingual
- * puzzle, followed by a new line."
- * return 0.
+ * puzzle, followed by a new line.
+ * Return: Always 0.
  */
 
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
